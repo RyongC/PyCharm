3bd34ade96f378fb86fccdfef319ec1177dc6fba
Seo <tjdydxo1213@gmail.com>
2022-08-16 23:02:24 +0900

    22.08.16

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-print()
-
-print('hi')
-
-# # i = 10 을 매개변수로 넣어준 덕에 디폴트 값이 생긴거임!
-# def hi(a, i=10):
-#     tmp = a + i
-#     return tmp
-#
-# print(hi(10, i=30))
-
-print('hi')
+print('hello, lia~!')
